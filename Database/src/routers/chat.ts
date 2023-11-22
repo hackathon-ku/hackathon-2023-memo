4225ad2f88efda5dae14bb310a27910e699956e5
Pilleteer <patcharapol.ra@ku.th>
2023-11-22 17:23:51 +0700

    Add createSchema and editSchema to chat router

--- Database/src/routers/chat.ts
+++ Database/src/routers/chat.ts
@@ -2,6 +2,7 @@ import express from 'express'
 import Chat from '../models/chat'
 import Message from '../models/message'
 import { async } from '@firebase/util'
+import { createSchema, editSchema } from '../schemas/chat'
 
 const router = express.Router()
 
@@ -11,12 +12,40 @@ router.get('/:chat_id', async (req, res) => {
         if (!chat_id) {
             return res.status(400).send('Body not match')
         }
-        const message = await Message.find({
-            chat_id: chat_id
-        })
-        if (!message) {
-            return res.status(400).send('Message not found')
+        const chat = await Chat.findById(chat_id).populate('messages')
+        if (!chat) {
+            return res.status(400).send('Chat not found')
         }
+        return res.status(200).send(chat.messages)
+    } catch (error) {
+        return res.status(500).send(error)
+    }
+})
+
+router.post('/create/:tid', async (req, res) => {
+    try{
+        const tid = req.params?.tid
+        if (!tid) {
+            return res.status(400).send('Body not match')
+        }
+
+        const createData = {
+            _id: tid,
+            messages: [],
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        }
+        const message = await Message.create({
+            //text: createData.data.text,
+            createdAt: new Date(),
+            updatedAt: new Date(),
+        })
+        const chat = await Chat.findById(tid)
+        if (!chat) {
+            return res.status(400).send('Chat not found')
+        }
+        chat.messages.push(message._id)
+        await chat.save()
         return res.status(200).send(message)
     } catch (error) {
         return res.status(500).send(error)
--- Database/src/schemas/chat.ts
+++ Database/src/schemas/chat.ts
@@ -1,1 +1,20 @@
 import { z } from 'zod'
+ 
+export const createSchema = z.object({
+    _id : z.string(),
+    name : z.string(),
+    messages : z.array(z.string()),
+    createdAt : z.date(),
+    updatedAt : z.date(),
+})
+
+export const editSchema = z.object({
+    _id : z.string(),
+    name : z.string().optional(),
+    messages : z.array(z.string()).optional(),
+    createdAt : z.date().optional(),
+    updatedAt : z.date().optional(),
+})
+
+export type createSchema = z.infer<typeof createSchema>
+export type editSchema = z.infer<typeof editSchema>
