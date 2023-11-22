2e7e33b48e09e3277183915631f5001caad32faa
VorakornK <vorakorn.ko@ku.th>
2023-11-22 12:17:28 +0700

    setup-fastapi

--- Backend/main.py
+++ Backend/main.py
@@ -1,18 +1,11 @@
-from typing import Union
-from pymongo import MongoClient
 from fastapi import FastAPI
+import requests
 import os
 import dotenv
 dotenv.load_dotenv()
 
 app = FastAPI()
-mongo = MongoClient(os.getenv("MONGODB"))
 
 @app.get("/")
 def read_root():
     return "Hello world!"
-
-
-@app.get("/items/{item_id}")
-def read_item(item_id: int, q: Union[str, None] = None):
-    return {"item_id": item_id, "q": q}
