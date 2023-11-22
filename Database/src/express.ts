f8e243ecc20da499979eb70f85c13f0c93c25e6e
Pilleteer <patcharapol.ra@ku.th>
2023-11-22 16:03:31 +0700

    Update express.ts file to include server start message

--- Database/src/express.ts
+++ Database/src/express.ts
@@ -49,3 +49,13 @@ app.get('/', (req, res) => {
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
+
+app.listen(port, () => {
+  console.log(
+    chalk.greenBright.bold(
+      `✔ Server started at ${chalk.underline.white(
+        `http://localhost:${port}`,
+      )}`,
+    ),
+  )
+})
