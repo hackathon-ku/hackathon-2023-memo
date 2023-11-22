a46ec46d8d5325de7d174c08f50c37ed54f0aa9e
Pilleteer <patcharapol.ra@ku.th>
2023-11-22 17:42:08 +0700

    Refactor chat creation endpoint and add message validation

--- Database/src/routers/chat.ts
+++ Database/src/routers/chat.ts
@@ -2,9 +2,18 @@ import express from 'express'
 import Chat from '../models/chat'
 import Message from '../models/message'
 import { async } from '@firebase/util'
-import { createSchema, editSchema } from '../schemas/chat'
+import { createSchema } from '../schemas/message'
+import { string } from 'zod'
 
 const router = express.Router()
+
+interface IChat {
+    _id: string;
+    name: string;
+    messages: string[];
+    createdAt: Date;
+    updatedAt: Date;
+}
 
 router.get('/:chat_id', async (req, res) => {
     try{
@@ -22,31 +31,42 @@ router.get('/:chat_id', async (req, res) => {
     }
 })
 
-router.post('/create/:tid', async (req, res) => {
+router.post('/create/:tid/:chatname', async (req, res) => {
     try{
         const tid = req.params?.tid
+        const chatname = req.params?.chatname
         if (!tid) {
             return res.status(400).send('Body not match')
         }
 
-        const createData = {
+        const chatData: IChat = {
             _id: tid,
+            name: chatname,
             messages: [],
             createdAt: new Date(),
             updatedAt: new Date(),
         }
-        const message = await Message.create({
-            //text: createData.data.text,
+        const messageData = createSchema.safeParse(req.body)
+        if (!messageData.success) {
+            return res.status(400).send('Body not match')
+        }
+        const msg = await Message.create({
+            username: messageData.data.username,
+            content: messageData.data.content,
+            isNotGPT: messageData.data.isNotGPT,
+            type: messageData.data.type,
+            chatid: tid,
             createdAt: new Date(),
             updatedAt: new Date(),
         })
-        const chat = await Chat.findById(tid)
+        if (!msg) {
+            return res.status(400).send('Message not found')
+        }
+        chatData.messages.push(msg._id)
+        const chat = await Chat.create(chatData)
         if (!chat) {
             return res.status(400).send('Chat not found')
         }
-        chat.messages.push(message._id)
-        await chat.save()
-        return res.status(200).send(message)
     } catch (error) {
         return res.status(500).send(error)
     }
