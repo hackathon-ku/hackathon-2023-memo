07c80df4ae9053a06033568b9666755e5427f9c0
Pilleteer <patcharapol.ra@ku.th>
2023-11-22 16:25:29 +0700

    update chat query

--- Database/src/routers/chat.ts
+++ Database/src/routers/chat.ts
@@ -1,1 +1,26 @@
+import express from 'express'
+import Chat from '../models/chat'
+import Message from '../models/message'
+import { async } from '@firebase/util'
 
+const router = express.Router()
+
+router.get('/:chat_id', async (req, res) => {
+    try{
+        const chat_id = req.params?.chat_id
+        if (!chat_id) {
+            return res.status(400).send('Body not match')
+        }
+        const message = await Message.find({
+            chat_id: chat_id
+        })
+        if (!message) {
+            return res.status(400).send('Message not found')
+        }
+        return res.status(200).send(message)
+    } catch (error) {
+        return res.status(500).send(error)
+    }
+})
+
+export default router
